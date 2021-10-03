308d1ac07a89a08bbb6848cbb92a21ee6d2bb16a
Juan Fonseca <juanma2268@gmail.com>
2021-10-02 22:17:18 -0600

    Creacion inicial

---
+++ ContactTesting/Tests/ConsumerPactTest.cs
@@ -0,0 +1,74 @@
+﻿using ContactTesting.Consumer;
+using ContactTesting.Mock;
+using PactNet.Mocks.MockHttpService;
+using PactNet.Mocks.MockHttpService.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ContactTesting.Tests
+{
+    public class ConsumerPactTest : IClassFixture<ConsumerPact>
+    {
+
+        private IMockProviderService _mockProviderService;
+        private string _mockProviderServiceBaseUri;
+
+
+        public ConsumerPactTest(ConsumerPact data)
+        {
+            _mockProviderService = data.MockProviderService;
+            _mockProviderService.ClearInteractions();
+            _mockProviderServiceBaseUri = data.MockProviderServiceBaseUri;
+        }
+
+
+        [Fact]
+        public void GetEmployeesDetails_VerifyIfItReturns()
+        {
+
+            //Arrange
+            _mockProviderService
+                .Given("Employee Details for Id '1'")
+                .UponReceiving("A GET request to retrieve the employee details")
+                .With(new ProviderServiceRequest
+                {
+
+                    Method = HttpVerb.Get,
+                    Path = "/employee/1",
+                    Headers = new Dictionary<string, object>
+                    {
+                        { "Accept", "application/json" }
+                    }
+
+
+                })
+                .WillRespondWith(new ProviderServiceResponse
+                {
+                    Status = 200,
+                    Headers = new Dictionary<string, object>
+                    {
+                        { "Content-Type", "application/json; charset=utf-8" }
+                    },
+                    Body = new //NOTE: Note the case sensitivity here, the body will be serialised as per the casing defined
+                    {
+                        id = 1,
+                        employeeName = "Karthik",
+                        email = "karthik@techgeek.co.in",
+                        city = "Auckland"
+                    }
+                });
+
+            //Act 
+            var consumer = new APIClient(_mockProviderServiceBaseUri);
+            var result = consumer.GetEmployeeDetails("1");
+
+            //Assert
+            Assert.Equal("Karthik", result.EmployeeName);
+
+            _mockProviderService.VerifyInteractions();
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # pactTestingKarthickCourse-gl
-Linux version adapted from https://gorillalogic.udemy.com/course/api-testing-with-restsharp-and-specflow-in-csharp/
+Linux version of Karthick's code provided on https://gorillalogic.udemy.com/course/api-testing-with-restsharp-and-specflow-in-csharp/
+
+## Employee API 
+* Run the API: `cd EmployeeAPI/;dotnet run`
+* Request:`https://localhost:5001/api/employee/1`
+* Response: `{"id":1,"employeeName":"Karthik","email":"karthik@techgeek.co.in","city":"Auckland"}`
+
+## Pact testing
+Execute on the root folder the command `dotnet build` to create files "service_consumer-employeelist.json" (the contract) and "employeelist_mock_service.log" (logs) in the /tmp directory. Then, execute `dotnet test`, the result should be something like this:
+
+```
+Iniciando la ejecución de pruebas, espere...
+1 archivos de prueba en total coincidieron con el patrón especificado.
+INFO  WEBrick 1.3.1
+INFO  ruby 2.2.2 (2015-04-13) [x86_64-linux]
+INFO  WEBrick::HTTPServer#start: pid=71687 port=9224
+
+Correctas! - Con error:     0, Superado:     2, Omitido:     0, Total:     2, Duración: 472 ms -
+```
